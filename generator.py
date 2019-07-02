b41c38a3518bd3aa3548817ebc1899afa00611d4
tikzoxs <tikzoxs@gmail.com>
2019-07-02 14:11:07 +1200

    before putting to the GPU machine

--- generator.py
+++ generator.py
@@ -4,12 +4,13 @@ import numpy as np
 
 class generator:
 	def __init__(self, file):
-		self.file = "/media/tkal976/Transcend/Tharindu/grey/Aeye_grey.h5"
+		self.file = "/media/tkal976/Transcend/Tharindu/grey/Aeye_grey_wrong_labels.h5"
+		# self.file = "/media/tkal976/Transcend/Tharindu/single_file/Aeye.h5"
 
 	def __call__(self):
 		with h5py.File(self.file, 'r') as h5f:
 			X_dset = h5f['X']
 			Y_dset = h5f['Y']
-			print(X_dset.shape[0])
+			print(Y_dset.shape)
 			for i in range(X_dset.shape[0]):
 				yield X_dset[i], np.argmax(Y_dset[i,:,0:5]), np.argmax(Y_dset[i,:,5:8]), np.argmax(Y_dset[i,:,8:11])
